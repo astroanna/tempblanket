9a4f4c89209cff9cf1a4be1ea416ede45caa8532
Anna <annagassen@me.com>
2019-04-24 15:32:11 -0700

    Adds colors to csv

--- tempblanket.js
+++ tempblanket.js
@@ -1,10 +1,33 @@
+
 var data = [];
 var promises = [];
 
-var start = new Date(2019, 03, 10); // 3 --> April
-var end = new Date(2019, 03, 11);
+var start = new Date(2018, 03, 12); // 3 --> April
+var end = new Date(2018, 04, 12);
 
-/*for (var d = new Date(start); d <= end; d.setDate(d.getDate() + 1)) {
+// Name of yarn color, lower end inclusive and higher end exclusive
+var colors = [
+	["Burgundy", 85, 95],
+	["Cranberry", 80, 85],
+	["Rust", 75, 80],
+	["Mustard", 70, 75],
+	["Lemon", 65, 70],
+	["Fern", 60, 65],
+	["Kelly Green", 55, 60],
+	["Sapphire", 50, 55],
+	["Colonial Blue", 45, 50],
+	["Purple", 40, 45],
+	["Eggplant", 30, 40]
+];
+
+$("#form").submit(function(event) {
+	console.log("hello");
+	alert("Handler for .submit() called.");
+	return false;
+});
+
+/*
+for (var d = new Date(start); d <= end; d.setDate(d.getDate() + 1)) {
 
 	url = 'https://cors-anywhere.herokuapp.com/'
 	+ 'https://api.darksky.net/forecast/'
@@ -24,14 +47,25 @@ axios.all(promises)
 	results.forEach(function (response) {
 		var dateObj = new Date(response.data.daily.data[0].time * 1000);
 		var date = (dateObj.getMonth() + 1) + '/' + dateObj.getDate() + '/' + dateObj.getFullYear();
+		
 		var minTemp = response.data.daily.data[0].temperatureMin;
 		var maxTemp = response.data.daily.data[0].temperatureMax;
 		var avgTemp = (minTemp + maxTemp) / 2;
-		data.push([date, minTemp, maxTemp, avgTemp]);
+		
+		var color = "";
+		
+		for (var i = 0; i < colors.length; i++) {
+			if ((maxTemp >= colors[i][1]) && (maxTemp < colors[i][2])) {
+				color = colors[i][0];
+				break;
+			}
+		}
+		
+		data.push([date, minTemp, maxTemp, avgTemp, color]);
 	})
 })
 .then( function () {
-    var csv = 'Date,Min Temp,Max Temp,Avg Temp\n';
+    var csv = 'Date,Min Temp,Max Temp,Avg Temp,Color\n';
     data.forEach(function(row) {
             csv += row.join(',');
             csv += "\n";
@@ -46,4 +80,5 @@ axios.all(promises)
 })
 .catch(function (error) {
 	console.log("error bitch");
-});*/
+});
+*/
